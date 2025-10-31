2c2b3bc562beb2d319816fc0f7a6168d8bb7a346
Gervig <gervig91@gmail.com>
2025-10-31 11:10:20 +0100

    mockdata attempt

--- src/main/java/dat/service/BoardGameGeekService.java
+++ src/main/java/dat/service/BoardGameGeekService.java
@@ -1,6 +1,8 @@
 package dat.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.databind.JsonNode;
 import dat.dto.GameDTO;
 
 import java.net.URI;
@@ -15,9 +17,9 @@ import java.util.List;
 // API2 Docs: https://boardgamegeek.com/wiki/page/BGG_XML_API2
 public class BoardGameGeekService
 {
-    private static final ObjectMapper objectMapper = new ObjectMapper();
     private static final HttpClient client = HttpClient.newHttpClient();
-    private static final String bggUri = "";
+    private static final XmlMapper xmlMapper = new XmlMapper();
+    private static final String bggUri = "https://boardgamegeek.com/xmlapi/collection/eekspider";  //TODO this is a temporary URI for getting mock-data
 
     public static List<GameDTO> getBGGGames()
     {
@@ -28,12 +30,27 @@ public class BoardGameGeekService
         {
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(new URI(bggUri))
+                    .header("User-Agent", "Mozilla/5.0") //TODO: should use API key, mimic browser
                     .GET()
                     .build();
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
             if (response.statusCode() == 200)
             {
                 //TODO handle data from BGG, 200 we got the data
+                String xml = response.body();
+
+                // Parse XML to a JsonNode tree
+                JsonNode rootNode = xmlMapper.readTree(xml);
+
+                // Loop over all <item> nodes
+                for (JsonNode itemNode : rootNode.path("item"))
+                {
+                    GameDTO game = xmlMapper.treeToValue(itemNode, GameDTO.class);
+                    gameDTOs.add(game);
+                }
+
+
             } else
             {
                 System.out.println("GET request failed. Status code: " + response.statusCode());
@@ -42,8 +59,6 @@ public class BoardGameGeekService
         {
             e.printStackTrace();
         }
-
-        //TODO create GameDTOs from BGG data
 
         return gameDTOs;
     }
