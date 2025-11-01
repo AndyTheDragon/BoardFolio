77a631dccd40c767359bf0a24c631d8827ee186e
Gervig <gervig91@gmail.com>
2025-11-01 21:29:32 +0100

    added lombok

--- src/main/java/dat/dto/GameDTO.java
+++ src/main/java/dat/dto/GameDTO.java
@@ -1,100 +1,24 @@
 package dat.dto;
 
 import dat.enums.Genre;
-
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import java.util.List;
 
+@Getter
+@Setter
+@ToString
 public class GameDTO
 {
+    private Long BGG_API_ID;
     private String title;
     private String description;
     private int minNoOfPlayers;
     private int maxNoOfPlayers;
+    private int minAge;
+    private int intMaxAge;
     private int releaseYear;
     private List<String> languages;
     private Genre genre;
-
-    public String getTitle()
-    {
-        return title;
-    }
-
-    public String getDescription()
-    {
-        return description;
-    }
-
-    public int getMinNoOfPlayers()
-    {
-        return minNoOfPlayers;
-    }
-
-    public int getMaxNoOfPlayers()
-    {
-        return maxNoOfPlayers;
-    }
-
-    public int getReleaseYear()
-    {
-        return releaseYear;
-    }
-
-    public List<String> getLanguages()
-    {
-        return languages;
-    }
-
-    public Genre getGenre()
-    {
-        return genre;
-    }
-
-    public void setTitle(String title)
-    {
-        this.title = title;
-    }
-
-    public void setDescription(String description)
-    {
-        this.description = description;
-    }
-
-    public void setMinNoOfPlayers(int minNoOfPlayers)
-    {
-        this.minNoOfPlayers = minNoOfPlayers;
-    }
-
-    public void setMaxNoOfPlayers(int maxNoOfPlayers)
-    {
-        this.maxNoOfPlayers = maxNoOfPlayers;
-    }
-
-    public void setReleaseYear(int releaseYear)
-    {
-        this.releaseYear = releaseYear;
-    }
-
-    public void setLanguages(List<String> languages)
-    {
-        this.languages = languages;
-    }
-
-    public void setGenre(Genre genre)
-    {
-        this.genre = genre;
-    }
-
-    @Override
-    public String toString()
-    {
-        return "GameDTO{" +
-                "title='" + title + '\'' +
-                ", description='" + description + '\'' +
-                ", minNoOfPlayers=" + minNoOfPlayers +
-                ", maxNoOfPlayers=" + maxNoOfPlayers +
-                ", releaseYear=" + releaseYear +
-                ", languages=" + languages +
-                ", genre=" + genre +
-                '}';
-    }
 }
