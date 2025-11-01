5707a8094a62e0914b338330a8ba734f451e76d5
Gervig <gervig91@gmail.com>
2025-11-01 21:30:25 +0100

    main testing

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -3,12 +3,10 @@ package dat;
 import dat.config.ApplicationConfig;
 import dat.config.HibernateConfig;
 import dat.controllers.SecurityController;
-import dat.dto.GameDTO;
 import dat.routes.Routes;
-import dat.service.BoardGameGeekService;
 import jakarta.persistence.EntityManagerFactory;
 
-import java.util.List;
+import java.io.IOException;
 
 
 public class Main
@@ -16,13 +14,14 @@ public class Main
     private final static EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
 
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
         SecurityController securityController = new SecurityController(emf);
         Routes routes = new Routes(securityController);
 
         //TODO fjern senere, tester om data bliver hentet
 //        List<GameDTO> gameDTOS = BoardGameGeekService.getBGGGamesFromFile();
+//        List<GameDTO> gameDTOS = BoardGameGeekService.getBGGGames();
 //        gameDTOS.forEach(game -> System.out.println(game.toString()));
 
         ApplicationConfig
