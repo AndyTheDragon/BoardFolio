b39dc7089f9a49d59d7f56e6af9fe1b42de50414
Casper Alexander Gervig <gervig91@gmail.com>
2025-11-03 13:13:41 +0100

    removed testing from main

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -2,39 +2,24 @@ package dat;
 
 import dat.config.ApplicationConfig;
 import dat.config.HibernateConfig;
-import dat.controllers.GameController;
 import dat.controllers.SecurityController;
-import dat.dto.GameDTO;
 import dat.routes.Routes;
-import dat.services.BoardGameGeekService;
 import dat.services.Populator;
 import jakarta.persistence.EntityManagerFactory;
 
-import java.io.IOException;
-import java.util.List;
 
-
-public class Main {
+public class Main
+{
     private final static EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
 
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception
+    {
         SecurityController securityController = new SecurityController(emf);
         Routes routes = new Routes(securityController);
 
-        //TODO fjern senere, tester om data bliver hentet
-//        List<GameDTO> gameDTOs = BoardGameGeekService.getBGGGamesFromFile();
-//        List<GameDTO> gameDTOs = BoardGameGeekService.fetchAllGames();
-//        gameDTOs.forEach(game -> System.out.println(game.toString()));
-
-        ApplicationConfig
-                .getInstance()
-                .initiateServer()
-                .setRoute(routes.getRoutes())
-                .handleException()
-                .setApiExceptionHandling()
-                .checkSecurityRoles()
-                .startServer(7070);
+        ApplicationConfig.getInstance().initiateServer().setRoute(routes.getRoutes()).handleException()
+                         .setApiExceptionHandling().checkSecurityRoles().startServer(7070);
 
         Populator.DevPopulator();
     }
