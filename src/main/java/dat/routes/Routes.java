6245633e0173886794b2f9f9e50d923394470661
Rolf <158441656+mrPrimeBeef@users.noreply.github.com>
2025-11-14 10:14:34 +0100

    update

--- src/main/java/dat/routes/Routes.java
+++ src/main/java/dat/routes/Routes.java
@@ -43,7 +43,7 @@ public class Routes
         return () -> {
             //TODO Update roles for routes
             get("/{username}", gameController::getGameListsForUser, Roles.ANYONE);
-            post("add", gameController::createGameList, Roles.ANYONE);
+            post("/add", gameController::createGameList, Roles.ANYONE);
             put("/update/{listID}", gameController::updateList, Roles.ANYONE);
             delete("/remove/{listID}", gameController::deleteUserList, Roles.ANYONE);
 //            get("/list/{username}/{uuid}", gameController::getGameListById, Roles.ANYONE);
