c9a06ff63e35a6b6313a13c97ce308e421f31b59
Crispy212 <97233936+Crispy212@users.noreply.github.com>
2025-11-17 11:12:43 +0100

    Created new CollectionController
    CollectionController has the add/removeGame functionallity, still need to impliment routes for the endpoints and testing of code

---
+++ src/main/java/dat/controllers/CollectionController.java
@@ -0,0 +1,92 @@
+package dat.controllers;
+
+import dat.config.HibernateConfig;
+import dat.dto.GameDTO;
+import dat.entities.Game;
+import dat.entities.GameList;
+import dat.entities.UserAccount;
+import io.javalin.http.Context;
+import io.javalin.http.NotFoundResponse;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+
+public class CollectionController {
+
+    private final EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
+
+    public void addToCollection(Context ctx) {
+        String username = ctx.queryParam("username");
+        GameDTO dto = ctx.bodyAsClass(GameDTO.class);
+
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+
+            UserAccount user = em.find(UserAccount.class, username);
+            if (user == null) {
+                throw new NotFoundResponse("User not found: " + username);
+            }
+
+            GameList collection = user.getMyCollection();
+
+            Game game = dto.toEntity(dto);
+
+            Game existingGame = em.createQuery(
+                            "SELECT g FROM Game g WHERE g.BGG_API_ID = :apiId", Game.class)
+                    .setParameter("apiId", dto.getBGG_API_ID())
+                    .getResultStream()
+                    .findFirst()
+                    .orElse(null);
+
+            Game persistentGame;
+            if (existingGame != null) {
+                persistentGame = existingGame;
+            } else {
+                em.persist(game);
+                persistentGame = game;
+            }
+
+            collection.addGame(persistentGame);
+
+            em.merge(user);
+
+            em.getTransaction().commit();
+            ctx.json(collection);
+
+        } finally {
+            em.close();
+        }
+    }
+
+    public void removeFromCollection(Context ctx) {
+        String username = ctx.queryParam("username");
+        long gameId = Long.parseLong(ctx.queryParam("gameId"));
+
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+
+            UserAccount user = em.find(UserAccount.class, username);
+            if (user == null) {
+                throw new NotFoundResponse("User not found");
+            }
+
+            GameList collection = user.getMyCollection();
+
+            Game game = em.find(Game.class, gameId);
+            if (game == null) {
+                throw new NotFoundResponse("Game not found");
+            }
+
+            collection.removeGame(game);
+
+            em.merge(user);
+
+            em.getTransaction().commit();
+            ctx.json(collection);
+
+        } finally {
+            em.close();
+        }
+    }
+}
---
+++ src/main/java/dat/dto/GamleListDTO.java
@@ -0,0 +1,15 @@
+package dat.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+
+public class GamleListDTO
+{
+    private Long gameId;
+    private int listId;
+}
